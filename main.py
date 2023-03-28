a2b956da74460c8c79e8f901e4db315b83d3b8cb
Arkadiusz Mazurek <66950904+AMFeniksPL@users.noreply.github.com>
2023-03-28 19:34:27 +0200

    Initial Velocity for planets

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Planet:
     counter = 0
     listOfPlanets = []
 
-    AU = 149.6e9
+    AU = 149.6e9 # KM
     G = 6.67428e-11
     SCALE = 250 / AU
     TIMESTEP = 3600 * 24
@@ -67,12 +67,14 @@ class Planet:
         self.y += self.velY * Planet.TIMESTEP
 
     def draw(self):
-        x = self.x * Planet.SCALE + SCREEN_WIDTH / 2
-        y = self.y * Planet.SCALE + SCREEN_HEIGHT / 2
+        x = (self.x - offsetX) * Planet.SCALE + SCREEN_WIDTH / 2
+        y = (self.y - offsetY) * Planet.SCALE + SCREEN_HEIGHT / 2
 
-        scaledRadius = self.radius * Planet.SCALE / (250/Planet.AU)
+        scaledRadius = self.radius * Planet.SCALE
 
+        pygame.draw.rect(screen, self.color, Rect(x, y-10, 1, 10))
         pygame.draw.circle(screen, self.color, (x, y), scaledRadius)
+
 
 
     @staticmethod
@@ -82,13 +84,17 @@ class Planet:
 
     @staticmethod
     def update_all_planets():
-        print(Planet.SCALE)
         for planet in Planet.listOfPlanets:
             planet.apply_forces()
 
     @staticmethod
     def change_scale(coefficient):
         Planet.SCALE *= 1.03 ** coefficient
+
+
+def lerp(start, stop, interpolation):
+    return start + (stop - start) * interpolation
+
 
 
 # Odpalenie modułów pygame
@@ -100,22 +106,43 @@ SCREEN_HEIGHT = 720
 # Parametry Screena
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+offsetX = offsetY = 0
+
 
 def main():
+    global offsetX
+    global offsetY
+
+    targetScale = 250 / Planet.AU
+
     # Zegar kontrolujący FPS-y
     clock = pygame.time.Clock()
 
-    sun = Planet(0, 0, 20, (255, 255, 0), 2.0e30)
+    sun = Planet(0, 0, 696342, (255, 255, 0), 2.0e30)
 
-    mercury = Planet(0.42 * Planet.AU, 0, 10, (0, 0, 200), 0.330e24)
-    venus = Planet(0.72 * Planet.AU, 0, 10, (0, 0, 200), 4.87e24)
-    earth = Planet(1 * Planet.AU, 0, 10, (0, 0, 200), 5.97e24)
-    mars = Planet(1.63 * Planet.AU, 0, 10, (0, 0, 200), 0.642e24)
+    mercury = Planet(0.42 * Planet.AU, 0, 2439, (0, 0, 200), 0.330e24)
+    venus = Planet(0.72 * Planet.AU, 0, 6051, (200, 200, 20), 4.87e24)
+    earth = Planet(1 * Planet.AU, 0, 6371, (0, 0, 200), 5.97e24)
+    mars = Planet(1.63 * Planet.AU, 0, 3389, (200, 50, 50), 0.642e24)
 
     # Pętla gry
     running = True
     while running:
+
+        keys = pygame.key.get_pressed()
+        if keys[K_w]:
+            offsetY -= 1/Planet.SCALE
+        if keys[K_s]:
+            offsetY += 1/Planet.SCALE
+        if keys[K_a]:
+            offsetX -= 1/Planet.SCALE
+        if keys[K_d]:
+            offsetX += 1/Planet.SCALE
+
+
+
         # LOGIKA GRY (w przyszłości):
+        Planet.SCALE = lerp(Planet.SCALE, targetScale, 0.1)
         Planet.update_all_planets()
 
 
@@ -128,16 +155,26 @@ def main():
 
 
         # Czekanie na kolejną klatkę
-        clock.tick(60)
+
 
         #Aktualizacja gry
         pygame.display.flip()
+        clock.tick(60)
+
+        left, middle, right = pygame.mouse.get_pressed()
+
+        moveMouse = pygame.mouse.get_rel()
+        if left:
+            offsetX = offsetX - moveMouse[0] / Planet.SCALE
+            offsetY = offsetY - moveMouse[1] / Planet.SCALE
+
+
+
 
         # Te cztery linijki pozwalają nam normalnie zamknąć program
         for event in pygame.event.get():
             if event.type == pygame.MOUSEWHEEL:
-                Planet.change_scale(event.y)
-
+                targetScale *= 1.4 ** event.y
 
             if event.type == pygame.QUIT:
                 running = False
